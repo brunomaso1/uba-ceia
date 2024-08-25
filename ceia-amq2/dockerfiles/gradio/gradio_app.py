20c7ae5095f6a0713da0c4182c07d5012ceef173
Simon <95500626+simon-rodriguez@users.noreply.github.com>
2024-08-25 23:04:47 +0200

    feat: 🎉 Front-End con Gradio completo.

--- ceia-amq2/dockerfiles/gradio/gradio_app.py
+++ ceia-amq2/dockerfiles/gradio/gradio_app.py
@@ -2,38 +2,40 @@ import gradio as gr
 import requests
 import datetime
 
-def predict(date, location, min_temp, max_temp, rainfall, evaporation, sunshine, wind_gust_dir,
+def predict(weather_date, location, min_temp, max_temp, rainfall, evaporation, sunshine, wind_gust_dir,
             wind_gust_speed, wind_dir_9am, wind_dir_3pm, wind_speed_9am, wind_speed_3pm,
             humidity_9am, humidity_3pm, pressure_9am, pressure_3pm, cloud_9am, cloud_3pm,
             temp_9am, temp_3pm, rain_today):
     
     print("In Prediction")
 
-    formatted_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-    response = requests.post("http://localhost:8800/predict/", json={
-        "Date": str(formatted_date),
-        "Location": location,
-        "MinTemp": min_temp,
-        "MaxTemp": max_temp,
-        "Rainfall": rainfall,
-        "Evaporation": evaporation,
-        "Sunshine": sunshine,
-        "WindGustDir": wind_gust_dir,
-        "WindGustSpeed": wind_gust_speed,
-        "WindDir9am": wind_dir_9am,
-        "WindDir3pm": wind_dir_3pm,
-        "WindSpeed9am": wind_speed_9am,
-        "WindSpeed3pm": wind_speed_3pm,
-        "Humidity9am": humidity_9am,
-        "Humidity3pm": humidity_3pm,
-        "Pressure9am": pressure_9am,
-        "Pressure3pm": pressure_3pm,
-        "Cloud9am": cloud_9am,
-        "Cloud3pm": cloud_3pm,
-        "Temp9am": temp_9am,
-        "Temp3pm": temp_3pm,
-        "RainToday": rain_today
-    })
+    json_msg = {
+        "features": {
+            "Date": weather_date,
+            "Location": location,
+            "MinTemp": min_temp,
+            "MaxTemp": max_temp,
+            "Rainfall": rainfall,
+            "Evaporation": evaporation,
+            "Sunshine": sunshine,
+            "WindGustDir": wind_gust_dir,
+            "WindGustSpeed": wind_gust_speed,
+            "WindDir9am": wind_dir_9am,
+            "WindDir3pm": wind_dir_3pm,
+            "WindSpeed9am": wind_speed_9am,
+            "WindSpeed3pm": wind_speed_3pm,
+            "Humidity9am": humidity_9am,
+            "Humidity3pm": humidity_3pm,
+            "Pressure9am": pressure_9am,
+            "Pressure3pm": pressure_3pm,
+            "Cloud9am": cloud_9am,
+            "Cloud3pm": cloud_3pm,
+            "Temp9am": temp_9am,
+            "Temp3pm": temp_3pm,
+            "RainToday": rain_today
+        }
+    }
+    response = requests.post("http://fastapi:8800/predict/", json=json_msg) 
     print(response.json())
     return response.json()['prediction_str']
 
@@ -42,7 +44,7 @@ wind_directions = ["E", "ENE", "ESE", "N", "NE", "NNE", "NNW", "NW", "S", "SE", 
 iface = gr.Interface(
     fn=predict,
     inputs=[
-        gr.DateTime(label="Date"),
+        gr.Text(label="Date (YYYY-MM-DD)"),
         gr.Dropdown(choices=[
             "Adelaide", "Albany", "Albury", "AliceSprings", "BadgerysCreek", "Ballarat",
             "Bendigo", "Brisbane", "Cairns", "Canberra", "Cobar", "CoffsHarbour",
@@ -79,4 +81,6 @@ iface = gr.Interface(
     title="Rain Prediction Model",
     description="Introduce los datos del tiempo de hoy para predecir si lloverá mañana."
 )
-iface.launch()
+
+print("Starting")
+iface.launch(server_name="0.0.0.0")
