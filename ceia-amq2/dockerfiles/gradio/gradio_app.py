a30072a80a3b7e8016d898a652abfcca0f7e7fe0
Bruno Masoller <brunomaso1@hotmail.com>
2024-08-25 19:47:59 -0300

    feat: :sparkles: GRADIO: Improve configs

--- ceia-amq2/dockerfiles/gradio/gradio_app.py
+++ ceia-amq2/dockerfiles/gradio/gradio_app.py
@@ -1,11 +1,34 @@
 import gradio as gr
 import requests
 import datetime
+import os
+
+FASTAPI_URL = os.environ.get('FASTAPI_URL')
+FASTAPI_PREDICT_ENDPOINT = FASTAPI_URL + "/predict/"
 
 def predict(weather_date, location, min_temp, max_temp, rainfall, evaporation, sunshine, wind_gust_dir,
             wind_gust_speed, wind_dir_9am, wind_dir_3pm, wind_speed_9am, wind_speed_3pm,
             humidity_9am, humidity_3pm, pressure_9am, pressure_3pm, cloud_9am, cloud_3pm,
             temp_9am, temp_3pm, rain_today):
+    
+    # Validaciones para campos requeridos
+    if not weather_date:
+        return "Error: La fecha es requerida."
+    
+    if not location:
+        return "Error: La ubicación es requerida."
+
+    if not wind_gust_dir:
+        return "Error: La dirección del viento en la ráfaga es requerida."
+
+    if not wind_dir_9am:
+        return "Error: La dirección del viento a las 9am es requerida."
+    
+    if not wind_dir_3pm:
+        return "Error: La dirección del viento a las 3pm es requerida."
+    
+    if rain_today is None:
+        return "Error: El campo 'Rain Today' es requerido."
     
     print("In Prediction")
 
@@ -35,16 +58,16 @@ def predict(weather_date, location, min_temp, max_temp, rainfall, evaporation, s
             "RainToday": rain_today
         }
     }
-    response = requests.post("http://fastapi:8800/predict/", json=json_msg) 
+    response = requests.post(FASTAPI_PREDICT_ENDPOINT, json=json_msg) 
     print(response.json())
     return response.json()['prediction_str']
 
 wind_directions = ["E", "ENE", "ESE", "N", "NE", "NNE", "NNW", "NW", "S", "SE", "SSE", "SSW", "SW", "W", "WNW", "WSW"]
 
-iface = gr.Interface(
+demo = gr.Interface(
     fn=predict,
     inputs=[
-        gr.Text(label="Date (YYYY-MM-DD)"),
+        gr.Text(label="Date (YYYY-MM-DD)"),  # Campo obligatorio
         gr.Dropdown(choices=[
             "Adelaide", "Albany", "Albury", "AliceSprings", "BadgerysCreek", "Ballarat",
             "Bendigo", "Brisbane", "Cairns", "Canberra", "Cobar", "CoffsHarbour",
@@ -55,16 +78,16 @@ iface = gr.Interface(
             "Richmond", "Sale", "SalmonGums", "Sydney", "SydneyAirport", "Townsville",
             "Tuggeranong", "Uluru", "WaggaWagga", "Walpole", "Watsonia", "Williamtown",
             "Witchcliffe", "Wollongong", "Woomera"
-        ], label="Location"),
+        ], label="Location"),  # Campo obligatorio
         gr.Number(label="Min Temp"),
         gr.Number(label="Max Temp"),
         gr.Number(label="Rainfall"),
         gr.Number(label="Evaporation"),
         gr.Number(label="Sunshine", minimum=0, maximum=24),
-        gr.Dropdown(choices=wind_directions, label="Wind Gust Direction"),
+        gr.Dropdown(choices=wind_directions, label="Wind Gust Direction"),  # Campo obligatorio
         gr.Number(label="Wind Gust Speed"),
-        gr.Dropdown(choices=wind_directions, label="Wind Direction 9am"),
-        gr.Dropdown(choices=wind_directions, label="Wind Direction 3pm"),
+        gr.Dropdown(choices=wind_directions, label="Wind Direction 9am"),  # Campo obligatorio
+        gr.Dropdown(choices=wind_directions, label="Wind Direction 3pm"),  # Campo obligatorio
         gr.Number(label="Wind Speed 9am"),
         gr.Number(label="Wind Speed 3pm"),
         gr.Number(label="Humidity 9am"),
@@ -83,4 +106,4 @@ iface = gr.Interface(
 )
 
 print("Starting")
-iface.launch(server_name="0.0.0.0")
+demo.launch(server_name="0.0.0.0")
