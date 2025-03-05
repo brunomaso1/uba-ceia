d37c04058ca8f9e0579ef9247c103e26cfa52d2a
brunomaso1 <brunomaso1@hotmail.com>
2025-03-05 19:25:38 -0300

    feat: :sparkles: Se agrega la aplicacion CVAT
    Se agrega la aplicación CVAT que estaba en el proyecto pero había faltado subir por temas del gitignore.

---
+++ ceia-proyecto-final/cvat/csrf-ddns-settings.py
@@ -0,0 +1,5 @@
+# Overlaying production
+from cvat.settings.production import *
+
+# CSRF_TRUSTED_ORIGINS = ['https://picudo-rojo.org', 'http://picudo-rojo.org']
+CSRF_TRUSTED_ORIGINS = ['https://picudo-rojo.org', 'http://picudo-rojo.org', 'http://cvat.picudo-rojo.org', 'https://cvat.picudo-rojo.org']
---
+++ ceia-proyecto-final/cvat/csrf-ngrok-settings.py
@@ -0,0 +1,4 @@
+# Overlaying production
+from cvat.settings.production import *
+
+CSRF_TRUSTED_ORIGINS = ['https://picudo-rojo-imm.ngrok.io', 'http://picudo-rojo-imm.ngrok.io']
