fc8e59049c6f173695535080e6466af39b4ac418
brunomaso1 <brunomaso1@hotmail.com>
2025-05-17 12:38:56 -0300

    fix: :boom: Modulo-mini-apps: Etiquetado + Webscrapping + Config
    Se realizan mejoras y cambios sobre la configuración.

--- ceia-proyecto-final/modulo-mini-apps/mini_apps_config/settings.py
+++ ceia-proyecto-final/modulo-mini-apps/mini_apps_config/settings.py
@@ -1,7 +1,10 @@
-import os, re, yaml
+from pathlib import Path
+import os, re, yaml, sys
 from typing import Any, Dict, Optional
 from yaml.parser import ParserError
-from utils import Singleton
+
+sys.path.append(os.path.abspath("../"))  # Agregar el directorio padre al path
+from mini_apps_utils.utils import Singleton
 
 tag_matcher = re.compile(r".*\${([^}^{]+)}.*")
 var_matcher = re.compile(r"\${([^}^{]+)}")
@@ -22,12 +25,17 @@ yaml.add_constructor("!envvar", path_constructor, yaml.SafeLoader)
 
 class Config(metaclass=Singleton):
     config_data: Dict = {}
-    yaml_filename = None
+    yaml_filename = "config.yaml"  # Ruta por defecto
 
-    def __init__(self, yaml_filename: str):
-        self.yaml_filename = yaml_filename
+    def __init__(self, yaml_filepath: Optional[Path] = None):
+        if yaml_filepath is None:
+            # Obtener la ruta al directorio donde se encuentra este archivo (settings.py)
+            settigs_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+            self.yaml_filepath = settigs_dir / self.yaml_filename  # Ruta por defecto
+        else:
+            self.yaml_filepath = yaml_filepath
         try:
-            with open(yaml_filename, "r") as f:
+            with open(self.yaml_filepath, "r") as f:
                 config_data = yaml.safe_load(f)
                 self.config_data = self.post_process_config(config_data)
         except (FileNotFoundError, PermissionError, ParserError) as e:
