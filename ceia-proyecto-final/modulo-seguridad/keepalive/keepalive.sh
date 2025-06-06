3b1a1ee418a90737a05a6fc37be1358ed1a7bcb4
brunomaso1 <brunomaso1@hotmail.com>
2025-06-06 16:51:03 -0300

    feat: :sparkles: modulo-seguriad: keepalive
    Se agrega un ping a la URL remota.

--- ceia-proyecto-final/modulo-seguridad/keepalive/keepalive.sh
+++ ceia-proyecto-final/modulo-seguridad/keepalive/keepalive.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-TARGET_URL=${TARGET_URL:-http://192.168.0.3}
+TARGET_LOCAL_URL=${TARGET_LOCAL_URL:-http://192.168.0.3}
+TARGET_URL=${TARGET_URL:-https://picudo-rojo.org}
 PING_INTERVAL=${PING_INTERVAL:-3600}
 
 TELEGRAM_TOKEN=${TELEGRAM_TOKEN}
@@ -16,12 +17,22 @@ send_telegram_alert() {
   fi
 }
 
-echo "Keepalive script started. Pinging $TARGET_URL every $PING_INTERVAL seconds."
+echo "Keepalive script started. Pinging $TARGET_LOCAL_URL and $TARGET_URL every $PING_INTERVAL seconds."
+
+send_telegram_alert "🔄 KeepAlive: Script iniciado a las $(date)."
 
 while true; do
+  # Ping local URL
+  echo "[INFO] Sending request to $TARGET_LOCAL_URL - $(date)"
+  if ! curl -s --max-time 10 "$TARGET_LOCAL_URL" > /dev/null; then
+    send_telegram_alert "⚠️ KeepAlive: No se pudo contactar con $TARGET_LOCAL_URL a las $(date)."
+  fi
+  sleep 5
+
+  # Ping remote URL
   echo "[INFO] Sending request to $TARGET_URL - $(date)"
   if ! curl -s --max-time 10 "$TARGET_URL" > /dev/null; then
-    send_telegram_alert "⚠️ KeepAlive: No se pudo contactar con $TARGET_URL a las $(date)"
+    send_telegram_alert "❗ KeepAlive: No se pudo contactar con $TARGET_URL a las $(date)."
   fi
   sleep "$PING_INTERVAL"
 done
